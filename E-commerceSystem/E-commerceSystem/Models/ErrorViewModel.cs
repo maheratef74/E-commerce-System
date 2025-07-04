7fad5dc85a7f5e72ca7de7eb1212baa6a643e8d8
Maher <maheratef600@gmail.com>
2025-07-04 15:24:10 +0300

    Add project setup

---
+++ E-commerceSystem/E-commerceSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace E_commerceSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
